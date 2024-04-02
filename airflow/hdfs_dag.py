c20ee49eb41f97a28e224b1d025d1a586e7cbaba
user.email like : "thedatascape@gmail.com <user.name like : "codeots-tds>
2024-04-02 17:25:16 -0400

    replaced deleted hdfs_dag with a copy because I accidently deleted.

--- airflow/hdfs_dag.py
+++ airflow/hdfs_dag.py
@@ -1,6 +1,6 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.decorators import dag, task
+from airflow.operators.python import PythonOperator
+from airflow.sensors.external_task import ExternalTaskSensor
 from datetime import datetime, timedelta
 import subprocess
 import sys
@@ -10,77 +10,115 @@ import load_data_to_db
 
 default_args = {
     'owner': 'CodeOTS',
-    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'start_date': datetime(2024,1,1),
+    'email_on_failure': False,
+    'email_on_retry': False,
     'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-}
+    'retry_delay': timedelta(minutes=5)
+    }
 
-def run_script(script_path):
-    subprocess.run(["bash", script_path], check=True)
-
-with DAG(
-    'dag_hdfs_processing',
+@dag(
+    dag_id='dag_hdfs_processing',
     default_args=default_args,
     schedule_interval='15 0 * * *',
-    catchup=False,
     tags=['crypto'],
-) as dag:
+)
+def dag_process_crypto_data():
 
-    wait_for_get_data = ExternalTaskSensor(
-        task_id='wait_for_get_data',
+    wait_for_fetch_crypto_data = ExternalTaskSensor(
+        task_id='wait_for_fetch_crypto_data',
         external_dag_id='dag_fetch_crypto_data',
         external_task_id='task_save_data',
-        check_existence=True,
-        timeout=600,  # seconds for timeout to wait for the task before retry
-        mode='reschedule',
-        poke_interval=15,
-    )
-
-    upload_datafiles_to_hadoop = PythonOperator(
-        task_id='upload_datafiles_to_hadoop',
-        python_callable=run_script,
-        op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/upload_to_hadoop.sh'},
+        timeout=1800,
+        poke_interval=60, # How often to check for the condition
+        allowed_states=['success'],
+        failed_states=['failed', 'skipped'],
+        mode='reschedule'
     )
     
-    run_hadoop_mapper_job = PythonOperator(
-        task_id='run_hadoop_job',
-        python_callable=run_script,
-        op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/run_hadoop_job.sh'},
-    )
+    def run_script(script_path):
+        subprocess.run(["bash", script_path], check=True)
 
-    download_datafiles_to_local = PythonOperator(
-        task_id='download_datafiles_to_local',
-        python_callable=run_script,
-        op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/download_to_local.sh'},
-    )
+    @task
+    def serialize_json_data():
+        run_script('/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/convert_json.sh')
 
-    serialize_json_files = PythonOperator(
-        task_id='serialize_json_files',
-        python_callable=run_script,
-        op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/convert_json.sh'},
-    )
+    wait_for_fetch_crypto_data >> serialize_json_data()
+dag_instance = dag_process_crypto_data()
+# default_args = {
+#     'owner': 'CodeOTS',
+#     'start_date': datetime(2023, 1, 1),
+#     'retries': 3,
+#     'retry_delay': timedelta(minutes=5),
+# }
 
-    delete_datafiles_from_local = PythonOperator(
-        task_id='delete_datafiles_from_local',
-        python_callable=run_script,
-        op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/delete_datafiles.sh'},
-    )
+# def run_script(script_path):
+#     subprocess.run(["bash", script_path], check=True)
+
+# with DAG(
+#     'dag_hdfs_processing',
+#     default_args=default_args,
+#     schedule_interval='15 0 * * *',
+#     catchup=False,
+#     tags=['crypto'],
+# ) as dag:
+
+#     wait_for_get_data = ExternalTaskSensor(
+#     task_id='wait_for_get_data',
+#     external_dag_id='dag_fetch_crypto_data',
+#     external_task_id='task_save_data',
+#     check_existence=True,
+#     execution_delta=timedelta(seconds=10),  # Adjust this based on actual timing between tasks
+#     timeout=600,  # seconds for timeout to wait for the task before retry
+#     mode='reschedule',
+#     poke_interval=15,
+#     )
+
+#     upload_datafiles_to_hadoop = PythonOperator(
+#         task_id='upload_datafiles_to_hadoop',
+#         python_callable=run_script,
+#         op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/upload_to_hadoop.sh'},
+#     )
+    
+#     run_hadoop_mapper_job = PythonOperator(
+#         task_id='run_hadoop_job',
+#         python_callable=run_script,
+#         op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/run_hadoop_job.sh'},
+#     )
+
+#     download_datafiles_to_local = PythonOperator(
+#         task_id='download_datafiles_to_local',
+#         python_callable=run_script,
+#         op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/download_to_local.sh'},
+#     )
+
+#     serialize_json_files = PythonOperator(
+#         task_id='serialize_json_files',
+#         python_callable=run_script,
+#         op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/convert_json.sh'},
+#     )
+
+#     delete_datafiles_from_local = PythonOperator(
+#         task_id='delete_datafiles_from_local',
+#         python_callable=run_script,
+#         op_kwargs={'script_path': '/home/ra-terminal/Desktop/portfolio_projects/crypto_project/scripts/delete_datafiles.sh'},
+#     )
 
 
-    def save_data_to_pgdb_task_fn():
-        load_data_to_db.process_directory(load_data_to_db.conn, load_data_to_db.cur, load_data_to_db.tablename, load_data_to_db.directory)
-        load_data_to_db.conn.commit()
-        load_data_to_db.cur.close()
-        load_data_to_db.conn.close()
+#     def save_data_to_pgdb_task_fn():
+#         load_data_to_db.process_directory(load_data_to_db.conn, load_data_to_db.cur, load_data_to_db.tablename, load_data_to_db.directory)
+#         load_data_to_db.conn.commit()
+#         load_data_to_db.cur.close()
+#         load_data_to_db.conn.close()
 
-    save_data_to_pgdb_task = PythonOperator(
-    task_id='save_data_to_pgdb_task',
-    python_callable=save_data_to_pgdb_task_fn,
-    dag=dag,
-)
+#     save_data_to_pgdb_task = PythonOperator(
+#     task_id='save_data_to_pgdb_task',
+#     python_callable=save_data_to_pgdb_task_fn,
+# )
 
-    wait_for_get_data >> serialize_json_files >> upload_datafiles_to_hadoop >> run_hadoop_mapper_job >> \
-    download_datafiles_to_local >> save_data_to_pgdb_task >> delete_datafiles_from_local
+#     wait_for_get_data >> serialize_json_files >> upload_datafiles_to_hadoop >> run_hadoop_mapper_job >> \
+#     download_datafiles_to_local >> save_data_to_pgdb_task >> delete_datafiles_from_local
 
 # from airflow import DAG
 # from airflow.decorators import dag, task
